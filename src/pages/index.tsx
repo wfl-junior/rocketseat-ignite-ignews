6ef1071c498498b87bee3880ac30dd790fe8f96a
Wallace Júnior <wflj1997@hotmail.com>
2022-06-26 12:20:47 -0300

    Página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,31 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import { Fragment } from "react";
+import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
     <Fragment>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>Hello World</h1>
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+
+          <h1>
+            News about <br /> the <span>React</span> world
+          </h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90/month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </Fragment>
   );
 };
