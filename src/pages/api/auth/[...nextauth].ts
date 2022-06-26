0b3e072f6d421e1afec46023eb81fcfa88e7e73d
Wallace Júnior <wflj1997@hotmail.com>
2022-06-26 15:30:34 -0300

    Salvando usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,9 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -10,4 +11,22 @@ export default NextAuth({
       authorization: "https://github.com/login/oauth/authorize?scope=read:user",
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        await fauna.query(
+          q.Create(q.Collection("users"), {
+            data: {
+              email: user.email,
+            },
+          }),
+        );
+
+        return true;
+      } catch (error) {
+        console.log({ error });
+        return false;
+      }
+    },
+  },
 });
