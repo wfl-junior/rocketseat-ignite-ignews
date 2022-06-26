9b681d01748863a4395350f53c4b015fd089cce5
Wallace Júnior <wflj1997@hotmail.com>
2022-06-26 17:41:48 -0300

    Ouvindo eventos do Stripe

--- .env.d.ts
+++ .env.d.ts
@@ -1,8 +1,9 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     STRIPE_API_KEY: string;
-    NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string;
     STRIPE_PRODUCT_PRICE_ID: string;
+    NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string;
+    STRIPE_WEBHOOK_SECRET: string;
     STRIPE_SUCCESS_URL: string;
     STRIPE_CANCEL_URL: string;
 
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,9 +1,58 @@
-import { NextApiHandler } from "next";
+import { NextApiHandler, PageConfig } from "next";
+import { Readable } from "stream";
+import { Stripe } from "stripe";
+import { stripe } from "../../services/stripe";
 
-const handler: NextApiHandler = async (_request, response) => {
-  console.log("evento recebido");
+async function getBuffer(readable: Readable) {
+  const chunks: any[] = [];
 
-  return response.status(200).json({ ok: true });
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+
+  return Buffer.concat(chunks);
+}
+
+export const config: PageConfig = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const relevantEvents = new Set(["checkout.session.completed"]);
+
+const handler: NextApiHandler = async (request, response) => {
+  if (request.method !== "POST") {
+    return response
+      .setHeader("Allow", "POST")
+      .status(405)
+      .end("Method not allowed");
+  }
+
+  const buffer = await getBuffer(request);
+  const secret = request.headers["stripe-signature"];
+
+  let event: Stripe.Event | undefined;
+
+  try {
+    if (!secret) {
+      throw new Error("no secret");
+    }
+
+    event = stripe.webhooks.constructEvent(
+      buffer,
+      secret,
+      process.env.STRIPE_WEBHOOK_SECRET,
+    );
+  } catch (error) {
+    return response.status(400).json(error);
+  }
+
+  if (relevantEvents.has(event.type)) {
+    console.log("Evento recebido", event);
+  }
+
+  return response.status(204).send();
 };
 
 export default handler;
