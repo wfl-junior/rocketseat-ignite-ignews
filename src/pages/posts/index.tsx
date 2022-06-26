85781cf8b9c10c3acae7a063e0b21b93ef48f8af
Wallace Júnior <wflj1997@hotmail.com>
2022-06-26 20:37:29 -0300

    Listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,29 +1,66 @@
 import Prismic from "@prismicio/client";
 import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { RichText } from "prismic-dom";
 import { Fragment } from "react";
 import { getPrismicClient } from "../../services/prismic";
 import styles from "../../styles/Posts.module.scss";
 
-export const getStaticProps: GetStaticProps = async () => {
+interface Content {
+  type: string;
+  text: string;
+}
+
+interface Post {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+  updatedAtFormatted: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export const getStaticProps: GetStaticProps<PostsProps> = async () => {
   const prismic = getPrismicClient();
 
   const response = await prismic.query(
-    Prismic.predicates.at("document.type", "Post"),
+    Prismic.predicates.at("document.type", "post"),
     {
-      fetch: ["Post.title", "Post.content"],
+      fetch: ["post.title", "post.content"],
       pageSize: 100,
     },
   );
 
-  console.log(response);
+  const posts = response.results.map(post => {
+    const updatedAt = new Date(post.last_publication_date!);
+
+    return {
+      slug: post.uid!,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        (post.data.content as Content[]).find(
+          content => content.type === "paragraph",
+        )?.text ?? "Sem texto",
+      updatedAt: updatedAt.toLocaleString(),
+      updatedAtFormatted: updatedAt.toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
+    };
+  });
 
   return {
-    props: {},
+    props: {
+      posts,
+    },
   };
 };
 
-const Posts: NextPage = () => (
+const Posts: NextPage<PostsProps> = ({ posts }) => (
   <Fragment>
     <Head>
       <title>Posts | ig.news</title>
@@ -31,38 +68,13 @@ const Posts: NextPage = () => (
 
     <main className={styles.container}>
       <div className={styles.posts}>
-        <a href="#">
-          <time dateTime={new Date("2021-03-12").toLocaleString()}>
-            12 de março de 2021
-          </time>
-
-          <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-
-          <p>
-            In this guide, you will learn how to create a Monorepo to manage
-            multiple packages with a shared build, test, and release process.
-          </p>
-        </a>
-        <a href="#">
-          <time>12 de março de 2021</time>
-
-          <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-
-          <p>
-            In this guide, you will learn how to create a Monorepo to manage
-            multiple packages with a shared build, test, and release process.
-          </p>
-        </a>
-        <a href="#">
-          <time>12 de março de 2021</time>
-
-          <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-
-          <p>
-            In this guide, you will learn how to create a Monorepo to manage
-            multiple packages with a shared build, test, and release process.
-          </p>
-        </a>
+        {posts.map(post => (
+          <a key={post.slug} href="#">
+            <time dateTime={post.updatedAt}>{post.updatedAtFormatted}</time>
+            <strong>{post.title}</strong>
+            <p>{post.excerpt}</p>
+          </a>
+        ))}
       </div>
     </main>
   </Fragment>
