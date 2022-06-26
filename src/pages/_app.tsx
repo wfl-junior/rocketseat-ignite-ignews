17900c67d7bd9ac3f4bad415c276809ff88f9a84
Wallace Júnior <wflj1997@hotmail.com>
2022-06-26 01:18:00 -0300

    Configurando fonte externa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { NextPage } from "next";
 import { AppProps } from "next/app";
+import Head from "next/head";
+import { Fragment } from "react";
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+
+      <Component {...pageProps} />
+    </Fragment>
+  );
 };
 
 export default App;
---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="crossorigin"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
