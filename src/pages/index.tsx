8a2c0247f5c5d1d3cc18c437773ba4106a880877
Wallace Júnior <wflj1997@hotmail.com>
2022-06-26 01:05:32 -0300

    Estilização com SASS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from "next";
+import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <h1 className={styles.title}>
+      Hello <span>World</span>
+    </h1>
+  );
 };
 
 export default Home;
