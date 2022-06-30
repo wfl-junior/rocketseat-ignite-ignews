b9d2289e98b09bb70f6ba22ac5554a74b19e8aa8
Wallace Júnior <wflj1997@hotmail.com>
2022-06-30 19:32:13 -0300

    Testando Header

---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import { Header } from ".";
+
+jest.mock("next/router", () => ({
+  useRouter: () => ({
+    asPath: "/",
+  }),
+}));
+
+jest.mock("next-auth/react", () => ({
+  useSession: () => [null, false],
+}));
+
+describe("Header component", () => {
+  it("should render correctly", () => {
+    render(<Header />);
+
+    expect(screen.getByText("Home")).toBeInTheDocument();
+    expect(screen.getByText("Posts")).toBeInTheDocument();
+  });
+});
